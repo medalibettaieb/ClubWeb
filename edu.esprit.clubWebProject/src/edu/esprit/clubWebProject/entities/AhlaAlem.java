a89af78960b18632184d272d1854605ec797d7ad
medalibettaieb <medali.bettaieb@esprit.ens.tn>
2012-12-12 13:58:37 +0100

    my first commit
    Signed-off-by: medalibettaieb <medali.bettaieb@esprit.ens.tn>

---
+++ edu.esprit.clubWebProject/src/edu/esprit/clubWebProject/entities/AhlaAlem.java
@@ -0,0 +1,13 @@
+package edu.esprit.clubWebProject.entities;
+
+public class AhlaAlem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
